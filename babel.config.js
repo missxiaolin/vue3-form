1b71d404ac93583822c97a3c2baf1e669a7f210d
xiaolin <462441355@qq.com>
2021-08-20 13:30:26 +0800

    * 配置

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    presets: [
+        '@vue/cli-plugin-babel/preset',
+    ],
+    "plugins": [
+        ["@ant-design-vue/babel-plugin-jsx", {
+            "transformOn": true,
+            "compatibleProps": true
+        }],
+        ["import", { "libraryName": "ant-design-vue", "libraryDirectory": "es", "style": "css" }] // `style: true` 会加载 less 文件
+    ]
+}
