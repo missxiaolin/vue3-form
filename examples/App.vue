ab6bdb04f71cf74b46c68e5f5ad4215697a03855
xiaolin <462441355@qq.com>
2021-08-22 16:14:19 +0800

    * vue3 项目搭建

---
+++ examples/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.menu {
+  margin-bottom: 20px;
+}
+.demo {
+  padding: 20px;
+}
+a {
+  margin: 10px;
+}
+</style>
---
+++ examples/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+
+
+const routes = [
+
+]
+
+const router = createRouter({
+    routes, // short for `routes: routes`
+    history: createWebHashHistory(),
+  });
+
+export default router;
