cd48122601cc1a516136650434432648609622b6
xiaolin <462441355@qq.com>
2021-08-23 20:21:01 +0800

    * add

--- examples/docs/demo.vue
+++ examples/docs/demo.vue
@@ -3,3 +3,13 @@
         ssss
     </div>
 </template>
+
+<script>
+import {reactive, toRefs} from 'vue';
+
+// render index
+import FormRender from '../../packages/index';
+export default {
+    
+}
+</script>
---
+++ packages/index.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import { toRefs, watch, reactive } from 'vue';
+
+
+export default {
+    props: {
+        schema: Object,
+        formData: Object
+    },
+
+    setup(props, { emit }) {
+        
+    }
+}
