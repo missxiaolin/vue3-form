e799d4a12bfcca4d26f824954c2f2e5e651c9cf7
xiaolin <462441355@qq.com>
2021-08-26 11:54:11 +0800

    * add

--- packages/index.jsx
+++ packages/index.jsx
@@ -11,6 +11,37 @@ export default {
     },
 
     setup(props, { emit }) {
+        if (!props.schema) return null;
+        const { formData, schema } = toRefs(props)
 
+        let data = resolve(props.schema, formData.value);
+        emit('on-change', data);
+        watch(formData, () => {
+            data = resolve(props.schema, formData.value);
+            emit('on-validate', getValidateList(data, props.schema));
+        });
+
+        watch(schema.value, () => {
+            data = resolve(props.schema, formData.value);
+            emit('on-change', data);
+        });
+
+        const handleChange = (key, val) => {
+            emit('on-change', clone(val));
+        };
+        return () => {
+            const Field = widgets[mapping[`${props.schema.type}${props.schema.format ? `:${props.schema.format}` : ''}`]];
+            return (
+                <div className="vue-form-render">
+                    <Field
+                        schema={props.schema}
+                        formData={data}
+                        value={data}
+                        name="$form"
+                        onChange={handleChange}
+                    />
+                </div>
+            )
+        }
     }
 }
